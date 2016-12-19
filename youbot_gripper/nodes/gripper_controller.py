bc43311aa5f66e8f88c75bb5aea7da6ec78491b4
ruudvd <ruud_dommelen@hotmail.com>
2016-12-19 15:12:55 +0100

    gripper werkend

--- youbot_gripper/nodes/gripper_controller.py
+++ youbot_gripper/nodes/gripper_controller.py
@@ -6,20 +6,25 @@ from std_srvs.srv import Empty, EmptyResponse
 
 gripper_open = 3.5
 gripper_close = 0.7
-max_torque = 0.1
-stepsize = 0.025
+max_torque = 0.2
+stepsize = 0.01
+global effort, currentpos
+effort = -1
+currentpos = 0
+effortcounter = 0
 
 WARN_TEMP = 60
 
-pub = rospy.Publisher("/gripper_controller/command", Float64, queue_size=10)
-sub = rospy.Subscriber("/gripper_controller/state", JointState , joint_states_callback)
 
 def joint_states_callback(msg):
-    global effort
-    effort = abs(msg.load[0])
-    temperatures = msg.motor_temps
+    global effort, currentpos
+    currentpos = msg.current_pos
+    effort = abs(msg.load)
+    temperatures = msg.motor_temps[0]
+    #print(moving)
     if temperatures > WARN_TEMP:
-        rospy.logerr("Temperature is getting hot of the gripper, %s", str(self.temperatures))
+	print(temperatures)
+        rospy.logerr("gripper is too hot!")
 
 def GripperController():
     service_open = rospy.Service("/open_gripper", Empty, openGripper)
@@ -30,26 +35,36 @@ def openGripper(req = None):
     if req is not None:
         return EmptyResponse()
 
-def closeGripper(req = None, max_torque):
+def closeGripper(req = None):
+    global effort, max_torque, currentpos
     gripper_feedback = gripper_open
-    for gripper_feedback in range(gripper_close, gripper_open):
-        if (is_goal_reached(effort, max_torque)):
-            break
-        send_command(gripper_feedback)
-        gripper_feedback -= stepsize
+    counter = 0.0
+    lasteffort = 0
+    while gripper_feedback > gripper_close and counter < 3:
+	if not(lasteffort ==  effort):
+            if effort > max_torque:
+   	        counter += 1.0
+	        lasteffort = effort
+            else:
+	        counter = 0.0
+	    lasteffort = effort
+	    print counter
+        error = abs(gripper_feedback-currentpos)
+        if error < 0.2 or gripper_feedback > currentpos:
+            send_command(gripper_feedback)
+            gripper_feedback -= stepsize
+    print "done closing"
     if req is not None:
         return EmptyResponse()
-
-def is_goal_reached(effort, max_torque):
-    global effort
-    if  (effort >  max_torque):
-        return True
-    return False
     
 def send_command(cmd):
     #rate = rospy.Rate(20)
     #print cmd
     pub.publish(cmd)
+
+pub = rospy.Publisher("/gripper_controller/command", Float64, queue_size=10)
+sub = rospy.Subscriber("/gripper_controller/state", JointState, joint_states_callback)
+
     
 if __name__ == '__main__':
     rospy.init_node("gripper_controller_action")
